58cb3bc9eb9931ae75ef7b755125d6f5571e579f
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 21:20:52 +0800

    pseudocode

--- exercise_9.py
+++ exercise_9.py
@@ -6,3 +6,7 @@
 # Check Palindrome Number
 # Write a program to check if the given number is a palindrome number.
 # A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
+
+# create a function
+
+# check if working
