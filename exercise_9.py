bde2481715207978cbe8562aec1e88a546a567e3
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 21:20:03 +0800

    description

---
+++ exercise_9.py
@@ -0,0 +1,8 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 9:
+# Check Palindrome Number
+# Write a program to check if the given number is a palindrome number.
+# A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
